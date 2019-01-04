963ad8470dedd88aa74fd8d7c389dd4bd8571bbc
Reuben <rjkoppy@gmail.com>
2019-01-04 13:00:33 -0500

    Rename snippets to weresquirrel

---
+++ chapter-4/weresquirrel.js
@@ -0,0 +1,33 @@
+let journal = [];
+
+function addEntry(events, squirrel) {
+  journal.push({ events, squirrel });
+}
+
+addEntry(["work", "touched tree", "pizza", "running", "television"], false);
+addEntry(
+  [
+    "work",
+    "ice cream",
+    "cauliflower",
+    "lasagna",
+    "touched tree",
+    "brushed teeth"
+  ],
+  false
+);
+addEntry(["weekend", "cycling", "break", "peanuts", "beer"], true);
+
+function phi(table) {
+  return (
+    (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt(
+      (table[2] + table[3]) *
+        (table[0] + table[1]) *
+        (table[1] + table[3]) *
+        (table[0] + table[2])
+    )
+  );
+}
+
+console.log(phi([76, 9, 4, 1]));
