96c313e991377105ac74260255b69d3f69484fa8
Reuben <rjkoppy@gmail.com>
2019-01-04 10:31:34 -0500

    Complete exercise 3.3

--- chapter-3/3-bean-counting.js
+++ chapter-3/3-bean-counting.js
@@ -20,3 +20,26 @@ console.log(countBs("BBC"));
 console.log(countChar("kakkerlak", "k"));
 → 4
 */
+
+const countBs = word => {
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == "B") {
+      count++;
+    }
+  }
+  return count;
+};
+
+const countChar = (word, char) => {
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == char) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(countBs("BtheBinB"));
+console.log(countChar("BtheBinB", "t"));
