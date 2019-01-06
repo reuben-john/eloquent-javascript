701f5f95ac6a8629628cd72858e6d915a1c35b58
Reuben <rjkoppy@gmail.com>
2019-01-06 10:51:16 -0500

    Complete exercise 4.1

--- chapter-4/1-sum-of-range.js
+++ chapter-4/1-sum-of-range.js
@@ -25,3 +25,30 @@ console.log(range(5, 2, -1));
 console.log(sum(range(1, 10)));
 → 55
 */
+
+function range(start, end, step = 1) {
+  let array = [];
+  if (step < 0) {
+    for (let i = start; i >= end; i += step) {
+      array.push(i);
+    }
+  } else {
+    for (let i = start; i <= end; i += step) {
+      array.push(i);
+    }
+  }
+  return array;
+}
+
+function sum(array) {
+  let total = 0;
+  for (let number of array) {
+    total += number;
+  }
+  return total;
+}
+
+console.log(range(1, 10));
+console.log(range(3, 12, 3));
+console.log(range(5, 2, -1));
+console.log(sum(range(1, 10)));
