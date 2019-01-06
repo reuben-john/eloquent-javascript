7cdef922094b2830009f8a2fff72c8fb10b3d805
Reuben <rjkoppy@gmail.com>
2019-01-06 07:54:26 -0500

    Finish weresquirrel journal

--- chapter-4/weresquirrel.js
+++ chapter-4/weresquirrel.js
@@ -60,9 +60,19 @@ function journalEvents(journal) {
 // console.log(tableFor("peanuts", JOURNAL));
 // console.log(journalEvents(JOURNAL));
 
-for (let event of journalEvents(JOURNAL)) {
-  let correlation = phi(tableFor(event, JOURNAL));
-  if (correlation > 0.1 || correlation < -0.1) {
-    console.log(event + ":", correlation);
+// for (let event of journalEvents(JOURNAL)) {
+//   let correlation = phi(tableFor(event, JOURNAL));
+//   if (correlation > 0.1 || correlation < -0.1) {
+//     console.log(event + ":", correlation);
+//   }
+// }
+
+for (let entry of JOURNAL) {
+  if (
+    entry.events.includes("peanuts") &&
+    !entry.events.includes("brushed teeth")
+  ) {
+    entry.events.push("peanut teeth");
   }
 }
+console.log(phi(tableFor("peanut teeth", JOURNAL)));
