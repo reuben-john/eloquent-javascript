48783f6ee8cf83b69ef3439fb5b770f2677f448c
Reuben <rjkoppy@gmail.com>
2019-01-06 09:10:22 -0500

    Add code snippets

--- chapter-4/snippets.js
+++ chapter-4/snippets.js
@@ -14,5 +14,39 @@ function rememberUrgently(task) {
 }
 
 remember("groceries");
-
+remember("nothing");
+rememberUrgently("this if first");
 console.log(todoList);
+
+let newList = todoList.slice(1);
+console.log(newList);
+
+function remove(array, index) {
+  return array.slice(0, index).concat(array.slice(index + 1));
+}
+
+console.log(remove([1, 2, 3, 4, 5], 2));
+
+// pad characters
+console.log(String(6).padStart(3, "x"));
+
+// rest parameters
+// use ...
+
+function max(...numbers) {
+  let result = -Infinity;
+  for (let number of numbers) {
+    if (number > result) result = number;
+  }
+  return result;
+}
+
+console.log(max(4, 1, 92, 32));
+
+// ... can aslo spread an array into individual numbers
+
+let numbers = [3, 5, 7];
+console.log(max(...numbers));
+
+// can spread an array into a new array
+console.log([1, 2, 3, ...numbers, 55, 23]);
