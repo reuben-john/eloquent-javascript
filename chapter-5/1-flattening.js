09fabb93594d55f83cc902f72bd510cda7e4df6d
Reuben <rjkoppy@gmail.com>
2019-01-22 13:58:07 -0500

    Finish exercise 1

--- chapter-5/1-flattening.js
+++ chapter-5/1-flattening.js
@@ -4,6 +4,6 @@
 // to “flatten” an array of arrays into a single array that has
 // all the elements of the original arrays.
 
-// let arrays = [[1, 2, 3], [4, 5], [6]];
-// Your code here.
-// → [1, 2, 3, 4, 5, 6]
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+console.log(arrays.reduce((a, b) => a.concat(b)));
