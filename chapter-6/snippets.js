43b92d46cf2e367c5dec0397c6e73ddef4179b08
Reuben <rjkoppy@gmail.com>
2019-01-22 15:18:46 -0500

    Add killerrabbit

---
+++ chapter-6/snippets.js
@@ -0,0 +1,10 @@
+let protoRabbit = {
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+};
+
+let killerRabbit = Object.create(protoRabbit);
+
+killerRabbit.type = "killer";
+killerRabbit.speak("Skreee!");
