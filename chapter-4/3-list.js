c4743c28571f70a125b2dbdef0e2b9099646a99a
Reuben <rjkoppy@gmail.com>
2019-01-22 11:56:25 -0500

    Finish problem 3

--- chapter-4/3-list.js
+++ chapter-4/3-list.js
@@ -43,3 +43,42 @@ console.log(prepend(10, prepend(20, null)));
 console.log(nth(arrayToList([10, 20, 30]), 1));
 → 20
 */
+
+function arrayToList(array) {
+  let list = null;
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    list = { value: array[i], rest: list };
+  }
+  return list;
+}
+
+function listToArray(list) {
+  let array = [];
+
+  // For loop in MDN
+  // for ([initialization]; [condition]; [final-expression])
+  // node = list, while node is true, push(value), then set node to node.rest
+
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+
+  return array;
+}
+
+function prepend(value, list) {
+  return { value, rest: list };
+}
+
+function nth(list, n) {
+  // loop through each object until value is returned
+  if (!list) return undefined;
+  else if (n == 0) return list.value;
+  else return nth(list.rest, n - 1);
+}
+
+console.log(arrayToList([1, 2, 3]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 1));
