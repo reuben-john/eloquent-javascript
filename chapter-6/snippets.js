26febdd0390cd6ea982fa9f2b2e65dc1a724ebb5
Reuben <rjkoppy@gmail.com>
2019-01-23 13:03:02 -0500

    add class

--- chapter-6/snippets.js
+++ chapter-6/snippets.js
@@ -8,3 +8,37 @@ let killerRabbit = Object.create(protoRabbit);
 
 killerRabbit.type = "killer";
 killerRabbit.speak("Skreee!");
+
+// class constructor
+
+function makeRabbit(type) {
+  let rabbit = Object.create(protoRabbit);
+  rabbit.type = type;
+  return rabbit;
+}
+
+function Rabbit(type) {
+  this.type = type;
+}
+
+Rabbit.prototype.speak = function(line) {
+  console.log(`The ${this.type} rabbit says '${line}'`);
+};
+
+let weirdRabbit = new Rabbit("weird");
+
+console.log(weirdRabbit);
+weirdRabbit.speak("yo");
+
+// e6 class
+
+class RabbitES6 {
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+}
+
+let blackRabbit = new RabbitES6("black");
