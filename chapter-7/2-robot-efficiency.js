6c109f05f6b3f9a5c05442bb9ae485735069f4ec
Reuben <rjkoppy@gmail.com>
2019-01-24 16:36:22 -0500

    Set up cahtper 7

---
+++ chapter-7/2-robot-efficiency.js
@@ -0,0 +1,10 @@
+// Robot efficiency
+
+// Can you write a robot that finishes the delivery task faster
+// than goalOrientedRobot? If you observe that robot’s behavior,
+// what obviously stupid things does it do? How could those be improved?
+
+// If you solved the previous exercise, you might want to use
+// your compareRobots function to verify whether you improved the robot.
+
+// runRobotAnimation(VillageState.random(), yourRobot, memory);
