6f6f5d6044e4d67747d0f75664d1290cb20120d0
Reuben <rjkoppy@gmail.com>
2019-01-24 14:47:46 -0500

    add matrix info

--- chapter-6/snippets.js
+++ chapter-6/snippets.js
@@ -63,3 +63,79 @@ ages.set("Julia", 69);
 // console.log(`Julia is ${ages.get("Julia")}`);
 // console.log("Is Jack's age known?", ages.has("Jack"));
 // console.log(ages.has("toString"));
+
+// Symbols are unique and cannot be created more than once
+
+// let sym = Symbol("name");
+// console.log(sym == Symbol("name"));
+// Rabbit.prototype[sym] = 55;
+// console.log(blackRabbit[sym]);
+
+const toStringSymbol = Symbol("toString");
+Array.prototype[toStringSymbol] = function() {
+  return `${this.length} cm of blue yarn`;
+};
+
+// console.log([1, 2].toString());
+// console.log([1, 2][toStringSymbol]());
+
+// Can refer to symbols using []
+
+let stringObject = {
+  [toStringSymbol]() {
+    return "a jute rope";
+  }
+};
+
+// console.log(stringObject[toStringSymbol]());
+
+// Symbol iterator can be used to go through iterable properies
+
+let okIterator = "OK"[Symbol.iterator]();
+// console.log(okIterator.next());
+// console.log(okIterator.next());
+// console.log(okIterator.next());
+
+class Matrix {
+  constructor(width, height, element = (x, y) => undefined) {
+    this.width = width;
+    this.height = height;
+    this.content = [];
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        this.content[y * width + x] = element(x, y);
+      }
+    }
+  }
+  get(x, y) {
+    return this.content[y * this.width + x];
+  }
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
+  }
+}
+
+class MatrixIterator {
+  constructor(matrix) {
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+
+  next() {
+    if (this.y == this.matrix.height) return { done: true };
+
+    let value = {
+      x: this.x,
+      y: this.y,
+      value: this.matrix.get(this.x, this.y)
+    };
+    this.x++;
+    if (this.x == this.matrix.width) {
+      this.x = 0;
+      this.y++;
+    }
+    return { value, done: false };
+  }
+}
