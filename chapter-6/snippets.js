baecaf9750cc46d5b2a0dfea01c08e2319d31a93
Reuben <rjkoppy@gmail.com>
2019-01-24 15:09:34 -0500

    Finish chapter 6

--- chapter-6/snippets.js
+++ chapter-6/snippets.js
@@ -139,3 +139,73 @@ class MatrixIterator {
     return { value, done: false };
   }
 }
+
+Matrix.prototype[Symbol.iterator] = function() {
+  return new MatrixIterator(this);
+};
+
+let matrix = new Matrix(2, 2, (x, y) => `value ${x},${y}`);
+// for (let { x, y, value } of matrix) {
+//   console.log(x, y, value);
+// }
+
+// getts and setters
+
+let varyingSize = {
+  get size() {
+    return Math.floor(Math.random() * 100);
+  }
+};
+
+// console.log(varyingSize.size);
+
+class Temperature {
+  constructor(celsius) {
+    this.celsius = celsius;
+  }
+  get fahrenheit() {
+    return this.celsius * 1.8 + 32;
+  }
+  set fahrenheit(value) {
+    this.celsius = (value - 32) / 1.8;
+  }
+
+  static fromFahrenheit(value) {
+    return new Temperature((value - 32) / 1.8);
+  }
+}
+
+let temp = new Temperature(22);
+// console.log(temp.fahrenheit);
+temp.fahrenheit = 86;
+// console.log(temp.celsius);
+
+// static inside class are stored on the constructor
+// and are called on the class, not the new instance of the class
+
+// inheritance is creating a new class that inherits properties and behavior
+
+class SymmetricMatrix extends Matrix {
+  constructor(size, element = (x, y) => undefined) {
+    super(size, size, (x, y) => {
+      if (x < y) return element(y, x);
+      else return element(x, y);
+    });
+  }
+
+  set(x, y, value) {
+    super.set(x, y, value);
+    if (x != y) {
+      super.set(y, x, value);
+    }
+  }
+}
+
+let matrix1 = new SymmetricMatrix(5, (x, y) => `${x},${y}`);
+console.log(matrix1.get(2, 3));
+
+// check if class derives from another class
+// console.log(new SymmetricMatrix(2) instanceof SymmetricMatrix);
+// console.log(new SymmetricMatrix(2) instanceof Matrix);
+// console.log(new Matrix(2, 2) instanceof SymmetricMatrix);
+// console.log([1] instanceof Array);
