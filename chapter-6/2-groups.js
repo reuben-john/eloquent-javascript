492ce276399efb9ab5fbc84536a14a0d5826f455
Reuben <rjkoppy@gmail.com>
2019-01-24 16:01:33 -0500

    Finish exercise 2

--- chapter-6/2-groups.js
+++ chapter-6/2-groups.js
@@ -20,16 +20,45 @@
 // as argument and creates a group that contains all the values
 // produced by iterating over it.
 
-// class Group {
-//   // Your code here.
-// }
-
 // let group = Group.from([10, 20]);
 // console.log(group.has(10));
-// // → true
 // console.log(group.has(30));
-// // → false
 // group.add(10);
 // group.delete(10);
 // console.log(group.has(10));
-// // → false
+
+class Group {
+  constructor() {
+    this.group = [];
+  }
+
+  add(value) {
+    if (!this.group.includes(value)) {
+      this.group.push(value);
+    }
+  }
+  delete(value) {
+    let index = this.group.indexOf(value);
+    if (index > -1) {
+      this.group.slice(index, 1);
+    }
+  }
+  has(value) {
+    return this.group.includes(value);
+  }
+
+  static from(iterable) {
+    let group = new Group();
+    for (let item of iterable) {
+      group.add(item);
+    }
+    return group;
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+console.log(group.has(30));
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
