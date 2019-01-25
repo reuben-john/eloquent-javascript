67986af658d831bca76c6162078c8ca6f98de7fb
Reuben <rjkoppy@gmail.com>
2019-01-24 19:12:00 -0500

    Implement runrobot

--- chapter-7/snippets.js
+++ chapter-7/snippets.js
@@ -61,3 +61,25 @@ class VillageState {
     }
   }
 }
+
+let first = new VillageState("Post Office", [
+  { place: "Post Office", address: "Alice's House" }
+]);
+let next = first.move("Alice's House");
+
+// console.log(next.place);
+// console.log(next.parcels);
+// console.log(first.place);
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
