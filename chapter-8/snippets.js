6736adfc683e8332835c9fc2b78d306c0936ad7d
Reuben <rjkoppy@gmail.com>
2019-01-29 12:51:35 -0500

    Finish chapter 8

--- chapter-8/snippets.js
+++ chapter-8/snippets.js
@@ -61,3 +61,80 @@ try {
 } catch (error) {
   console.log("Something went wrong: " + error);
 }
+
+// error cleanup has flow control
+
+const accounts = {
+  a: 100,
+  b: 0,
+  c: 20
+};
+
+function getAccount() {
+  let accountName = prompt("Enter an account name");
+  if (!accounts.hasOwnProperty(accountName)) {
+    throw new Error(`No such account: ${accountName}`);
+  }
+  return accountName;
+}
+
+// function transfer(from, amount) {
+//   if (accounts[from] < amount) return;
+//   accounts[from] -= amount;
+//   accounts[getAccount()] += amount;
+// }
+
+// If error is thrown, the amount is deducted from the first account
+// but never transferred to the second
+
+// better version below
+
+function transfer(from, amount) {
+  if (accounts[name] < amount) return;
+  let progress = 0;
+  try {
+    accounts[from] -= amount;
+    progress = 1;
+    accounts[getAccount()] += amount;
+    progress = 2;
+  } finally {
+    if (progress == 1) {
+      accounts[from] += amount;
+    }
+  }
+}
+
+// It's not worth trying to blanket catch all errors, as catch will lcatch ALL errors
+// It is better to define a new type of error then test for that error
+
+// this empty class beahaves identically to error but has a new name
+class InputError extends Error {}
+
+function promptDirection(question) {
+  let result = prompt(question);
+  if (result.toLowerCase() == "left") return "L";
+  if (result.toLowerCase() == "right") return "R";
+  throw new InputError("Invalid direction: " + result);
+}
+
+for (;;) {
+  try {
+    let dir = promptDirection("Where?");
+    console.log("You chose ", dir);
+    break;
+  } catch (e) {
+    if (e instanceof InputError) {
+      console.log("Not a valid direction. Try again.");
+    } else {
+      throw e;
+    }
+  }
+}
+
+// Assertions are checks inside functions that directly throw errors when something happens
+function firstElement(array) {
+  if (array.length == 0) {
+    throw new Error("firstElement called with []");
+  }
+  return array[0];
+}
