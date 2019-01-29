aa7ddbec9541c45e017e93862b40b4ff0742d995
Reuben <rjkoppy@gmail.com>
2019-01-29 10:11:27 -0500

    Comment out section of code

--- chapter-8/2-locked-box.js
+++ chapter-8/2-locked-box.js
@@ -2,20 +2,20 @@
 
 // Consider the following (rather contrived) object:
 
-const box = {
-  locked: true,
-  unlock() {
-    this.locked = false;
-  },
-  lock() {
-    this.locked = true;
-  },
-  _content: [],
-  get content() {
-    if (this.locked) throw new Error("Locked!");
-    return this._content;
-  }
-};
+// const box = {
+//   locked: true,
+//   unlock() {
+//     this.locked = false;
+//   },
+//   lock() {
+//     this.locked = true;
+//   },
+//   _content: [],
+//   get content() {
+//     if (this.locked) throw new Error("Locked!");
+//     return this._content;
+//   }
+// };
 
 // It is a box with a lock. There is an array in the box, but you can get
 // at it only when the box is unlocked. Directly accessing the private _content
