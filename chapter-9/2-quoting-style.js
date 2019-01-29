294db75a0ab60d098aedeefe4f4923372117f184
Reuben <rjkoppy@gmail.com>
2019-01-29 16:36:25 -0500

    Finish exercise 2

--- chapter-9/2-quoting-style.js
+++ chapter-9/2-quoting-style.js
@@ -10,5 +10,5 @@
 
 let text = "'I'm the cook,' he said, 'it's my job.'";
 // Change this call.
-console.log(text.replace(/A/g, "B"));
+console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
 // → "I'm the cook," he said, "it's my job."
