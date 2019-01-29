a6a1178391fde302f206dfffd9e6b799bb85c975
Reuben <rjkoppy@gmail.com>
2019-01-29 15:45:50 -0500

    Add name replacement

--- chapter-9/snippets.js
+++ chapter-9/snippets.js
@@ -14,6 +14,43 @@ console.log(
     "$2 $1"
   )
 );
-// → Barbara Liskov
-//   John McCarthy
-//   Philip Wadler
+
+// Can also pass in function to replace
+let s = "the cia and fbi";
+console.log(s.replace(/\b(fbi|cia)\b/g, str => str.toUpperCase()));
+
+// another one
+
+let stock = "1 lemon, 2 cabbages, and 10 eggs";
+function minusOne(match, amount, unit) {
+  amount = Number(amount) - 1;
+  if (amount == 1) {
+    // only one left, remove the 's'
+    unit = unit.slice(0, unit.length - 1);
+  } else if (amount == 0) {
+    amount = "no";
+  }
+  return amount + " " + unit;
+}
+console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+
+// Use a non greedy operator when possible.
+// a ? after makes it non greedy
+function stripComments(code) {
+  return code.replace(/\/\/.*|\/\*[^]*?\*\//g, "");
+}
+console.log(stripComments("1 /* a */+/* b */ 1"));
+// → 1 + 1
+
+// let name = "harry";
+// let text = "Harry is a suspicious character.";
+// let regexp = new RegExp("\\b(" + name + ")\\b", "gi");
+// console.log(text.replace(regexp, "_$1_"));
+// → _Harry_ is a suspicious character.
+
+let name = "dea+hl[]rd";
+let text = "This dea+hl[]rd guy is super annoying.";
+let escaped = name.replace(/[\\[.+*?(){|^$]/g, "\\$&");
+let regexp = new RegExp("\\b" + escaped + "\\b", "gi");
+console.log(text.replace(regexp, "_$&_"));
+// → This _dea+hl[]rd_ guy is super annoying.
