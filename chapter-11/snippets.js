3cbf43068a0963a5f93c30d2856e0ec51ed88ba8
Reuben <rjkoppy@gmail.com>
2019-01-31 14:24:41 -0500

    Add more to async crow network

--- chapter-11/snippets.js
+++ chapter-11/snippets.js
@@ -55,3 +55,37 @@ new Promise((_, reject) => reject(new Error("Fail")))
     return "nothing";
   })
   .then(value => console.log("Handler 2", value));
+
+class Timeout extends Error {}
+
+function request(nest, target, type, content) {
+  return new Promise((resolve, reject) => {
+    let done = false;
+    function attempt(n) {
+      nest.send(target, type, content, (failed, value) => {
+        done = true;
+        if (failed) reject(failed);
+        else resolve(Value);
+      });
+      setTimeout(() => {
+        if (done) return;
+        else if (n < 3) attempt(n + 1);
+        else reject(new Timeout("Timed out"));
+      }, 250);
+    }
+    attempt(1);
+  });
+}
+
+function requestType(name, handler) {
+  defineRequestType(name, (nest, content, source, callback) => {
+    try {
+      Promise.resolve(handler(nest, content, source)).then(
+        response => callback(null, response),
+        failure => callback(failure)
+      );
+    } catch (exception) {
+      callback(exception);
+    }
+  });
+}
