322800c80f735caa2020df6afeea6153d6bdeb21
Reuben <rjkoppy@gmail.com>
2019-02-01 07:32:48 -0500

    add findRoute

--- chapter-11/snippets.js
+++ chapter-11/snippets.js
@@ -148,3 +148,16 @@ everywhere(nest => {
   nest.state.connections.set(nest.name, nest.neighbors);
   broadcastConnections(nest, nest.name);
 });
+
+function findRoute(from, to, connections) {
+  let work = [{ at: from, via: null }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, via } = work[i];
+    for (let next of connections.get(at) || []) if (next == to) return via;
+    if (next == to) return via;
+    if (!work.some(w => w.at == next)) {
+      work.push({ at: next, via: via || next });
+    }
+  }
+  return null;
+}
