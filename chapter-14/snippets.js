65f8d828006ed77b91c9f261b4089192e5a921ec
Reuben <rjkoppy@gmail.com>
2019-02-02 15:29:45 -0500

    Finish chapter 14

--- chapter-14/snippets.js
+++ chapter-14/snippets.js
@@ -97,3 +97,25 @@ time("clever", () => {
   let total = Math.ceil(2000 / (target.offsetWidth / 5));
   target.firstChild.nodeValue = "X".repeat(total);
 });
+
+function count(selector) {
+  return document.querySelectorAll(selector).length;
+}
+
+// console.log(count("p")); // All <p> elements
+// console.log(count(".animal")); // Class animal
+// console.log(count("p .animal")); // Animal inside of <p>
+// console.log(count("p > .animal")); // Direct child of <p>
+
+let cat = document.querySelector("img");
+let angle = Math.PI / 2;
+
+function animate(time, lastTime) {
+  if (lastTime != null) {
+    angle += (time - lastTime) * 0.001;
+  }
+  cat.style.top = Math.sin(angle) * 20 + "px";
+  cat.style.left = Math.cos(angle) * 200 + "px";
+  requestAnimationFrame(newTime => animate(newTime, time));
+}
+requestAnimationFrame(animate);
